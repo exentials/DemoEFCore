d0031307ae9b302dc479fd04a310732d4cd0a2d5
Damiano <damiano.g@outlook.it>
2019-08-18 22:54:15 +0200

    Added command line options

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.CommandLineUtils;
 
 namespace DemoEFCore
 {
@@ -7,19 +8,45 @@ namespace DemoEFCore
     {
         static void Main(string[] args)
         {
-            var optionsBuilder = new DbContextOptionsBuilder<DemoEFCoreContext>();
-            optionsBuilder.UseNpgsql($"Server=192.168.74.10;Port=5432;Database=DemoEFCore;User Id=postgres;Password = dbpass;");
-
-            using var db = new DemoEFCoreContext(optionsBuilder.Options);
-            db.Database.EnsureDeleted();    // ensure to delete the database if present
-            db.Database.EnsureCreated();    // ensure the database creation
-
-            // add some data to Master and Detail
-            db.Add(new TableMaster
+            var app = new CommandLineApplication();
+            var dbHostOption = app.Option("--db-host <host>", "database host", CommandOptionType.SingleValue);
+            var dbPortOption = app.Option("--db-port <port>", "database host port", CommandOptionType.SingleValue);
+            var dbNameOption = app.Option("--db-name <name>", "database name", CommandOptionType.SingleValue);
+            var dbPasswordOption = app.Option("--db-pass <password>", "database password", CommandOptionType.SingleValue);
+            app.HelpOption("-? | -h | --help");
+            app.OnExecute(() =>
             {
-                InsertDate = DateTime.Now,
-                MasterKey = "KEY001",
-                Details = new TableDetail[] {
+                string dbHost = dbHostOption.HasValue() ? dbHostOption.Value() : string.Empty;
+                string dbPort = dbPortOption.HasValue() ? dbPortOption.Value() : "5432";
+                string dbName = dbNameOption.HasValue() ? dbNameOption.Value() : "DemoEFCore";
+                string dbPassword = dbPasswordOption.HasValue() ? dbPasswordOption.Value() : "dbpass";
+                if (dbHost == string.Empty)
+                {
+                    app.Error.WriteLine("Database host option is missing");
+                    return 1;
+                }
+
+                var optionsBuilder = new DbContextOptionsBuilder<DemoEFCoreContext>();
+                optionsBuilder.UseNpgsql($"Server={dbHost};Port={dbPort};Database={dbName};User Id=postgres;Password={dbPassword};");
+
+                using var db = new DemoEFCoreContext(optionsBuilder.Options);
+                // ensure to delete the database if present
+                if (db.Database.EnsureDeleted())
+                {
+                    Console.WriteLine($"Database {dbName} deleted.");
+                }
+                // ensure the database creation
+                if (db.Database.EnsureCreated())
+                {
+                    Console.WriteLine($"Database {dbName} created.");
+                }
+
+                // add some data to Master and Detail
+                db.Add(new TableMaster
+                {
+                    InsertDate = DateTime.Now,
+                    MasterKey = "KEY001",
+                    Details = new TableDetail[] {
                     new TableDetail {
                         InsertDate = DateTime.Now,
                         DetailCode = "Code001",
@@ -31,9 +58,12 @@ namespace DemoEFCore
                         Quantity = 9
                     }
                 }
-            });
+                });
+                db.SaveChanges();   // save the changes
 
-            db.SaveChanges();   // save the changes
+                return 0;
+            });
+            app.Execute(args);
         }
     }
 }
